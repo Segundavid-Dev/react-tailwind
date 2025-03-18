dab9c450c64d954ad84f9a71244707398093161e
Segundavid-Dev <segdavid03@gmail.com>
2025-03-18 20:14:29 +0100

    created Input task component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 function Header() {
   return (
     <div className="flex w-full items-center justify-between">
-      <h1 className="text-4xl tracking-[.5rem]">TODO</h1>
+      <h1 className="text-4xl tracking-[.5rem] font-extrabold">TODO</h1>
       <img
         src="images/icon-sun.svg"
         alt="Sun Icon"
@@ -26,8 +26,13 @@ function Header() {
 
 function FormInput() {
   return (
-    <div>
-      <p>hello</p>
+    <div className="w-full relative">
+      <input
+        type="text"
+        placeholder="Create a new todo..."
+        className="bg-[var(--input-background)] w-full pt-[10px] pb-[10px] pl-[4rem] border-none outline-none text-[14px]"
+      />
+      <div className="absolute left-3 top-1.5 w-6 h-6 border-1 border-gray-500 rounded-full cursor-pointer"></div>
     </div>
   );
 }
