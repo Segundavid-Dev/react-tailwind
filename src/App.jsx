6ed7bbd7f1fd4737ed0359d23be26050860d2c1e
Segundavid-Dev <segdavid03@gmail.com>
2025-03-18 17:02:01 +0100

    Got the layout structure of my form with tailwind classes

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,38 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p>hello world</p>
+    <div className="flex flex-col w-[30vw] min-h-[100vh] items-center justify-center mx-auto">
+      <Header />
+      <FormInput />
+      <DisplayTask />
+      <Task />
     </div>
   );
 }
+
+function Header() {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <h1 className="text-4xl tracking-[.5rem]">TODO</h1>
+      <img
+        src="images/icon-sun.svg"
+        alt="Sun Icon"
+        className="cursor-pointer"
+      />
+    </div>
+  );
+}
+
+function FormInput() {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+}
+
+function DisplayTask() {}
+
+function Task() {}
 
 export default App;
