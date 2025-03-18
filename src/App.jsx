207f3478d7c561ee937dde08e91b21d8901dff79
Segundavid-Dev <segdavid03@gmail.com>
2025-03-18 21:50:18 +0100

    created DisplayTask component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
 import "./App.css";
 
+const initialTask = [
+  {
+    id: 1,
+    tasks: "Completed online Javascript Course",
+    status: "completed",
+  },
+  {
+    id: 2,
+    tasks: "Jog around the park 3x",
+    status: "Active",
+  },
+  {
+    id: 3,
+    tasks: "10 minutes meditation",
+    status: "Active",
+  },
+  {
+    id: 4,
+    tasks: "Read for 1 hour",
+    status: "Active",
+  },
+  {
+    id: 5,
+    tasks: "Pick up groceries",
+    status: "Active",
+  },
+  {
+    id: 6,
+    tasks: "Complete Todo App on frontend Mentor",
+    status: "Active",
+  },
+];
+
 function App() {
   return (
-    <div className="flex flex-col w-[30vw] min-h-[100vh] items-center justify-center mx-auto">
+    <div className="flex flex-col min-w-[30vw] sm:max-w-[40vw] md:max-w-[50vw] min-h-[100vh] items-center justify-center mx-auto">
       <Header />
       <FormInput />
       <DisplayTask />
@@ -30,15 +63,29 @@ function FormInput() {
       <input
         type="text"
         placeholder="Create a new todo..."
-        className="bg-[var(--input-background)] w-full pt-[10px] pb-[10px] pl-[4rem] border-none outline-none text-[14px]"
+        className="bg-[var(--input-background)] w-full pt-[15px] pb-[15px] pl-[4rem] border-none outline-none text-[14px] mb-[1rem]"
       />
       <div className="absolute left-3 top-1.5 w-6 h-6 border-1 border-gray-500 rounded-full cursor-pointer"></div>
     </div>
   );
 }
 
-function DisplayTask() {}
+function DisplayTask() {
+  return (
+    <div className="w-full relative bg-[var(--input-background)]">
+      {initialTask.map((task) => (
+        <Task />
+      ))}
+    </div>
+  );
+}
 
-function Task() {}
+function Task() {
+  return (
+    <div className="pt-[15px] pb-[15px] pl-[4rem] border-1 border-gray-500">
+      <p>display Task</p>
+    </div>
+  );
+}
 
 export default App;
