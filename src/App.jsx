afcc19ea905b7f7b37aa1d88ea2a68c1cf2bff97
Segundavid-Dev <segdavid03@gmail.com>
2025-03-19 09:43:14 +0100

    Set form elements as controlled elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const initialTask = [
@@ -16,28 +17,23 @@ const initialTask = [
     tasks: "10 minutes meditation",
     status: "Active",
   },
-  {
-    id: 4,
-    tasks: "Read for 1 hour",
-    status: "Active",
-  },
-  {
-    id: 5,
-    tasks: "Pick up groceries",
-    status: "Active",
-  },
-  {
-    id: 6,
-    tasks: "Complete Todo App on frontend Mentor",
-    status: "Active",
-  },
 ];
 
 function App() {
+  const [newTask, setNewTask] = useState("");
+
+  function handleInput(e) {
+    e.preventDefault();
+  }
+
   return (
     <div className="flex flex-col min-w-[30vw] sm:max-w-[40vw] md:max-w-[50vw] min-h-[100vh] items-center justify-center mx-auto">
       <Header />
-      <FormInput />
+      <FormInput
+        newTask={newTask}
+        setNewTask={setNewTask}
+        handleInput={handleInput}
+      />
       <DisplayTask />
       <FilterTask />
     </div>
@@ -57,16 +53,18 @@ function Header() {
   );
 }
 
-function FormInput() {
+function FormInput({ newTask, setNewTask, handleInput }) {
   return (
-    <div className="w-full relative">
+    <form className="w-full relative" onSubmit={handleInput}>
       <input
         type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
         placeholder="Create a new todo..."
         className="bg-[var(--input-background)] w-full pt-[15px] pb-[15px] pl-[4rem] border-none outline-none text-[14px] mb-[1rem]"
       />
       <div className="absolute left-3 top-3 w-6 h-6 border-1 border-gray-500 rounded-full cursor-pointer"></div>
-    </div>
+    </form>
   );
 }
 
